f75ff5ae3b560ad78bfeab1af7fb0b1ac79afae1
m-Cho1 <mturtle051@gmail.com>
2023-06-01 16:48:24 -0700

    quick javascript review before building the momentum app

--- Documents/momentum/app.js
+++ Documents/momentum/app.js
@@ -1,1 +1,28 @@
-alert("hi");
+const $title = document.querySelector('.title');
+
+isTitleClicked = true;
+
+function clickTitle() {
+ isTitleClicked = !isTitleClicked;
+  if (isTitleClicked) {
+    $title.style.color = "gold";
+
+  } else {
+    $title.style.color = 'red';
+  }
+}
+
+$title.addEventListener('click', clickTitle);
+
+function mouseEnter() {
+  $title.innerHTML = 'Mouse is here!'
+  console.log('the mouse has entered');
+}
+
+function mouseLeave() {
+  $title.innerHTML = 'Mouse left!'
+  console.log('the mouse has left');
+}
+
+$title.addEventListener('mouseenter', mouseEnter);
+$title.addEventListener('mouseleave', mouseLeave);
