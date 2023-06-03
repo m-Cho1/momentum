ff545ff67af47a1aaadfca7bb302f0c0d9c75806
m-Cho1 <mturtle051@gmail.com>
2023-06-02 18:45:06 -0700

    created a clock in main page

--- Documents/momentum/JS/clock.js
+++ Documents/momentum/JS/clock.js
@@ -2,7 +2,10 @@ const $clock = document.getElementById('clock');
 
 function getTime() {
   const date = new Date();
-  $clock.innerText = (`${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()}`)
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  $clock.innerText = `${String(hours).padStart(2, "0")} : ${String(minutes).padStart(2, "0")} : ${String(seconds).padStart(2, "0")}`;
 }
 
 getTime();
