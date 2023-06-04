0a9682310cfdbf5172d78ea85fa37cd46a0791ee
m-Cho1 <mturtle051@gmail.com>
2023-06-03 17:29:06 -0700

    added bac random background image feature

---
+++ Documents/momentum/JS/background.js
@@ -0,0 +1,16 @@
+const images = [
+  "0.jpg",
+  "1.jpg",
+  "2.jpg",
+  "3.jpg",
+  "4.jpg",
+  "5.jpg",
+  "6.jpg"
+];
+
+const randomImg = images[Math.floor(Math.random() * images.length)];
+
+const $bgImage = document.createElement('img');
+$bgImage.src = `img/${randomImg}`;
+
+document.body.appendChild($bgImage);
