1fcb9eead5666260ed89de873627cf971fe1b72b
m-Cho1 <mturtle051@gmail.com>
2023-06-05 19:52:37 -0700

    working on deleting todo

--- Documents/momentum/JS/todos.js
+++ Documents/momentum/JS/todos.js
@@ -2,10 +2,18 @@ const $todoForm = document.getElementById('todo-form');
 const $todoList = document.getElementById('todo-list');
 const $todoInput = $todoForm.querySelector('input');
 
+function deleteTodo(event) {
+  console.log('delete!', event.target.parentElement.innerText)
+}
+
 function renderTodo(newTodo) {
   const li = document.createElement('li');
   const span = document.createElement('span');
+  const button = document.createElement('button');
+  button.innerText = "❌";
+  button.addEventListener('click', deleteTodo)
   li.appendChild(span);
+  li.appendChild(button);
   span.innerText = newTodo;
   $todoList.appendChild(li);
 }
