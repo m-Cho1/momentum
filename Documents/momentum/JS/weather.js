fab5f32171afc1d5a27157ffbf7093c23393e99d
m-Cho1 <mturtle051@gmail.com>
2023-06-07 11:49:58 -0700

    created function to retrieve current location and weather using API

--- Documents/momentum/JS/weather.js
+++ Documents/momentum/JS/weather.js
@@ -1,12 +1,26 @@
+const API_KEY = "3452575787388279129325243f4018fe";
+
 function onGeoSuccess(position) {
   const latitude = position.coords.latitude;
   const longitude = position.coords.longitude;
-  console.log('position: ', 'lat: ',latitude, 'lon: ',longitude);
-  console.log('geolocation')
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=imperial`;
+
+  fetch(url)
+  .then(response => response.json())
+  .then((data) => {
+    const weather = document.querySelector('.weather');
+    const location = document.querySelector('.location');
+    const temperature = document.querySelector('.temperature');
+    const name = data.name;
+    weather.innerText = data.weather[0].main;
+    location.innerText = data.name;
+    temperature.innerText = data.main.temp + " ℉";
+    console.log(name, weather)
+  });
 }
 
-function onGeoErr() {
-  alert('Cannot find location. No weather available.')
+https: function onGeoErr() {
+  alert("Cannot find location. No weather available.");
 }
 
 
